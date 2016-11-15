6f8d43e1af6cf41310443bfa2fdbf58528f7e309
Joshua Cook <me@joshuacook.me>
2016-11-15 13:34:54 -0800

    RUNTIME simply run script

--- run.sh
+++ run.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 set -e
 . activate carnd_term_1
-
-if [ -z "$1" ]
-  then
-    jupyter notebook 
-elif [ "$1" == *".ipynb"* ]    
-  then
-    jupyter notebook "$1"
-else
-    exec "$@"
-fi
-
+exec "$@"
