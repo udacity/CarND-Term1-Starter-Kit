f5e053c0c9fc77444e2b180b3f048a343235dc74
domluna <dluna132@gmail.com>
2016-11-22 21:17:29 -0500

    update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 set -e
 . activate carnd_term_1
-exec "$@"
+
+if [ -z "$1" ]
+  then
+    jupyter notebook
+elif [ "$1" == *".ipynb"* ]
+  then
+    jupyter notebook "$1"
+else
+    exec "$@"
+fi
+
