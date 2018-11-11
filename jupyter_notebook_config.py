be7527c25e1819a47e1762220d01ffe1c68e4215
François Robinet <francois.rbnt@gmail.com>
2018-11-11 10:43:48 +0100

    quickfix for the now broken ip='*' Jupyter config

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,7 +1,7 @@
 import os
 from IPython.lib import passwd
 
-c.NotebookApp.ip = '*'
+c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = int(os.getenv('PORT', 8888))
 c.NotebookApp.open_browser = False
 c.MultiKernelManager.default_kernel_name = 'python'
